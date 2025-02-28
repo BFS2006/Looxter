4b60377c0dd3c4d81d958b1354ec60ee876e9f4d
Bryan Felipe Da Silva <bryanfelipedasilva7@gmail.com>
2025-02-28 15:57:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ document.getElementById("toggleButton").addEventListener("click", function() {
             area.style.display = "none";
         }, 500);
     } else {
+        var area1 = document.getElementById("areaToggle2");
+        if(area1.style.display === "flex") {
+            area1.style.animation = "togglecima 0.5s ease-in-out forwards";
+        
+            setTimeout(function() {
+            area1.style.display = "none";
+            }, 500);
+
+            area.style.display = "flex";
+            area.style.animation = "toggleDireita 0.5s ease-in-out forwards";
+
+        } else {
+            area.style.display = "flex";
+            area.style.animation = "toggleDireita 0.5s ease-in-out forwards";
+        }
         area.style.display = "flex";
         area.style.animation = "toggleDireita 0.5s ease-in-out forwards";
     }
@@ -43,8 +58,9 @@ function logadoHome() {
             fotoPerfil.innerHTML = "";
 
             fotoPerfil.innerHTML = `
-            <img src="${foto}" class="imagemPerfil" alt="">
-            <span style="font-size: 15px;">${nome}</span>
+            <img id="paraPerfil" src="${foto}" class="imagemPerfil" style="cursor: pointer; alt="" onclick="irPerfil()">
+
+            <span id="paraPerfil" style="font-size: 15px; cursor: pointer;" onclick="irPerfil()">${nome}</span>
             `
 
             // // Cria um novo elemento de imagem
@@ -61,10 +77,51 @@ function logadoHome() {
             var nome = localStorage.getItem("nome")
             console.log("Nenhuma foto de perfil encontrada.");
             fotoPerfil.innerHTML = "";
-            fotoPerfil.innerHTML = `<i class="bi bi-person-circle"></i><span>${nome}</span>`
+            fotoPerfil.innerHTML = `<i class="bi bi-person-circle" cursor: pointer;" onclick="irPerfil()"></i><span cursor: pointer;" onclick="irPerfil()">${nome}</span>`
         }
     } else {
         console.log("Usuário não está logado.");
+    }
+}
+
+function irPerfil() {
+    var area1 = document.getElementById("areaToggle2");
+
+    if (area1.style.display === "flex") {
+        area1.style.animation = "togglecima 0.5s ease-in-out forwards";
+        
+        setTimeout(function() {
+            area1.style.display = "none";
+        }, 500);
+    } else {
+        var area = document.getElementById("areaToggle");
+        if(area.style.display === "flex") {
+            area.style.animation = "toggleEsquerda 0.5s ease-in-out forwards";
+        
+            setTimeout(function() {
+            area.style.display = "none";
+            }, 500);
+
+            area1.style.display = "flex";
+            area1.style.animation = "togglebaixo 0.5s ease-in-out forwards";
+
+        } else {
+            area1.style.display = "flex";
+            area1.style.animation = "togglebaixo 0.5s ease-in-out forwards";
+        }
+    }
+}
+
+function Tperfil() {
+    var logadoP = localStorage.getItem("logado");
+    
+    if (logadoP === "true") {
+        console.log("Usuário está logado.");
+        irPerfil()
+        // Aqui você pode fazer o que precisar para exibir informações do usuário, etc.
+    } else {
+        console.log("Usuário não está logado.");
+        // Aqui você pode redirecionar para a página de login, se necessário
     }
 }
 
@@ -81,6 +138,18 @@ window.onload = function() {
     }
 }
 
+function redirecionarPerfil() {
+    window.location.href = "./TelaPerfil/index.html";
+}
+
+function deslogar() {
+    var logado1 = localStorage.getItem("logado");
+
+    if(logado1 === "true") {
+        localStorage.clear()
+        location.reload();
+    }
+}
 
 // function logadoHome() {
 //     var logado = localStorage.getItem("logado");
